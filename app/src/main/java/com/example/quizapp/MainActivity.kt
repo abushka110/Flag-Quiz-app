38e55d0b4df690f4db85b50e724ede69f4388760
abushka110 <qyjodixilu@gmail.com>
2023-06-26 00:20:26 +0300

    clickOnListener function for transition from the login window to questions

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val nameInput = findViewById<EditText>(R.id.name_input)
+        btnStart.setOnClickListener {
+            if(nameInput.text.isNotEmpty()) {
+                Toast.makeText(this, "Enter your name!", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
